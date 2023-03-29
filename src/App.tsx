546274d838cbfc92f529f7902b9b547a43936a41
Arshvir <arshvirc@gmail.com>
2023-03-29 16:39:51 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="relative flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
